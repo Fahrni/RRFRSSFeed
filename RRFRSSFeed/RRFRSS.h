5ea09988af015b00ad0ac89e7e328a6f37654f8d
Rob Fahrni <fahrni@agrian.com>
2018-04-12 00:49:12 -0700

    Updated to include Foundation.h

--- RRFRSSFeed/RRFRSS.h
+++ RRFRSSFeed/RRFRSS.h
@@ -5,6 +5,7 @@
 //  Created by Rob Fahrni on 8/2/14.
 //  Copyright (c) 2014 Hayseed. All rights reserved.
 //
+#import <Foundation/Foundation.h>
 #import "RRFRSSFeedTypes.h"
 #import "RRFRSSFeed.h"
 #import "RRFRSSChannel.h"
--- RRFRSSFeed/RRFRSSFeed.m
+++ RRFRSSFeed/RRFRSSFeed.m
@@ -5,6 +5,7 @@
 //  Created by Rob Fahrni on 8/2/14.
 //  Copyright (c) 2014 Hayseed. All rights reserved.
 //
+#import <Foundation/Foundation.h>
 #import "RRFRSSConst.h"
 #import "RRFRSSFeed.h"
 #import "RRFRSSItem.h"
